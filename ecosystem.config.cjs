441d09978ddd853f2609cdb83f4fa6c41034673d
supercz-trade <superczbsc9@gmail.com>
2026-04-23 03:07:52 +0800

    update pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,13 +2,12 @@ module.exports = {
   apps: [
     {
       name:         "quantx",
-      script:       "server.js",
+      script:       "src/server.js",
       interpreter:  "node",
 
-      cron_restart:       "*/30 * * * *", // restart setiap 30 menit
+      cron_restart:       "0 */6 * * *", // restart setiap 30 menit
       max_memory_restart: "2000M",         // restart jika memory > 2GB
       autorestart:        true,
-      max_restarts:       10,
       min_uptime:         "10s",
       restart_delay:      3000,
 
