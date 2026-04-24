b0e3e7aa69a354bf2369938ea340455b4e614d33
supercz-trade <superczbsc9@gmail.com>
2026-04-25 04:10:40 +0800

    update add origin

--- src/server.js
+++ src/server.js
@@ -121,7 +121,7 @@ async function main() {
   // ============================================================
 
   await fastify.register(cors, {
-    origin: ["https://supercz.pro","http://localhost:5173"],
+    origin: ["https://supercz.pro","http://localhost:5173","http://127.0.0.1:5173"],
     methods: ["GET","POST","PUT","DELETE","OPTIONS"],
     allowedHeaders: ["Content-Type","Authorization"],
     credentials: false
